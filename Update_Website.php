e4e9ec3cc58f8bbc7494a83cb7d64a540d6d9470
DavidUsyu <david.usyu@strathmore.edu>
2024-07-08 15:15:26 +0300

    commit

---
+++ Update_Website.php
@@ -0,0 +1,12 @@
+<?php
+    require(connection.php);
+    include(functions.php);
+    
+    if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST[Update_Website.php])) {
+        //something was posted
+        $user_name = $_POST['user_name'];
+        $password = $_POST['password'];
+        $user_email = $_POST['user_email'];
+        
+        $up
+        }
