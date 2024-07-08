f30daf154f0802b03f6260a2729ad45d78f92726
DavidUsyu <david.usyu@strathmore.edu>
2024-07-08 14:38:26 +0300

    membershipphp

--- membership.php
+++ membership.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,13 +12,13 @@
         <div class="menu">
             <ul>
                 <!--Creation of icons-->
-                <li><a href="index.html">HOME</a></li>
-                <li><a href="about.html">ABOUT</a></li>
-                <li><a href="service.html">SERVICE</a></li>
-                <li><a href="design.html">DESIGN</a></li>
-                <li><a href="games.html">GAMES</a></li>
-                <li><a href="membership.html">MEMBERSHIP</a></li>
-                <li><a href="contact.html">CONTACT</a></li>
+                <li><a href="Home.php">HOME</a></li>
+                <li><a href="about.php">ABOUT</a></li>
+                <li><a href="service.php">SERVICE</a></li>
+                <li><a href="design.php">DESIGN</a></li>
+                <li><a href="games.php">GAMES</a></li>
+                <li><a href="membership.php">MEMBERSHIP</a></li>
+                <li><a href="signup.php">SIGNUP</a></li>
             </ul>
         </div>
     <div class="document">
@@ -26,13 +27,13 @@
     </div>
     <div class="form">
         <h2>Login Here</h2>
+        <form method="post">
         <input type="email"name="email"placeholder="Enter Email Here"><p></p>
         <input type="password"name=""placeholder="Enter Password Here"><br>
-
         <button class="btnn"><a href="#">Login</a></button>
 
-        <p class="link">Don't have an account?<br>
-        <a href="#">Sign up</a>here</a></p>
+        <p class="link">Dont have an account?<br>
+        <a href="signup.php">Sign up</a>here</a></p>
         <p class="liw">Log in with</p>
         <div class="icons">
             <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
@@ -41,6 +42,7 @@
             <a href="#"><ion-icon name="logo-google"></ion-icon></a>
             <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
         </div>
+        </form>
     </div>
     </div>
     <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
