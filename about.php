39fe03157cba03edd2beb694d884d3f1d4863b50
DavidUsyu <david.usyu@strathmore.edu>
2024-07-09 00:31:40 +0300

    aboutphp

--- about.php
+++ about.php
@@ -13,10 +13,10 @@
                 <!--Creation of icons-->
                 <li><a href="index.php">HOME</a></li>
                 <li><a href="about.php">ABOUT</a></li>
-                <li><a href="service.php">SERVICE</a></li>
                 <li><a href="design.php">DESIGN</a></li>
                 <li><a href="games.php">GAMES</a></li>
                 <li><a href="membership.php">MEMBERSHIP</a></li>
+                <li><a href="update.php">UPDATE</a></li>
                 <li><a href="signup.php">SIGNUP</a></li>
             </ul>
         </div>
